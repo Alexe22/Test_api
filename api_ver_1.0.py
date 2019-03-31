824c78882a19c20b5b6202176e699aab93b62d3d
Alexey Nikiforov <kefir2204nikiforov@gmail.com>
2019-03-31 20:52:34 +0300

    Change sting format and delete unused variable

--- api_ver_1.0.py
+++ api_ver_1.0.py
@@ -18,27 +18,23 @@ URL = config.get('urls', 'URL')
 
 # получения номера телефона в регионе Москва
 
-
-# резервирования телефонного номера
 URL_RES = config.get('urls', 'URL_RES')
 
 request_res = requests.get(URL) #выполняем запрос
 
-code = request_res.status_code
-
-if code == 200:
+if request_res.status_code == 200:
     data = request_res.json()
     #print(data)
     if data['IsSuccess'] == True:
         number = data['Data']['Msisdn']
         uniq_id = data['Data']['Id']
-        print('Получен следующий простой номер телефона ', number)
+        print('Получен следующий простой номер телефона {0}'.format(number))
     else:
         print('Не удалось получить номер!')
-        print('Ошибка ', data['ErrorReason'])
-        print(data['ErrorMessage'])
+        print('Ошибка {0}'.format(data['ErrorReason']))
+        print('Текст ошибки: {0}'.format(data['ErrorMessage']))
 else:
-    print('Получен код возврата ', code)
+    print('Получен код возврата {0} '.format(request_res.status_code))
     print('Ошибка выполнения запроса!')
 
 
@@ -53,14 +49,14 @@ response = requests.post(URL_RES, data=data_json, headers=headers) #выполн
 if response.status_code == 200: # проверяем статус возрата сообщения
     result = response.json()
     if result['IsSuccess'] == True and result['Data']['IsCompleteReserve'] == True:
-        print('Телефонный номер', number, 'успешно зарезервирован')
+        print('Телефонный номер {0}, успешно зарезервирован'.format(number))
         token = result['Data']['ReservationToken']
     else:
         print('Не удалось зарезервировать номер!')
-        print('Ошибка ', result['ErrorReason'])
-        print(result['ErrorMessage'])
+        print('Ошибка {0}'.format(result['ErrorReason']))
+        print('Текст ошибки: {0}'.format(result['ErrorMessage']))
 else:
-    print('Получен код возврата ', response.status_code)
+    print('Получен код возврата {0}'.format(response.status_code))
     print('Ошибка выполнения запроса!')
 
 
@@ -95,11 +91,11 @@ if response_order.status_code == 200:
     result_order = response_order.json()
     #print(result_order)
     if result_order['IsSuccess'] == True:
-        print('Получен заказ', result_order['Data']['ReservationResult']['ReservationToken'])
+        print('Получен заказ {0}'.format(result_order['Data']['ReservationResult']['ReservationToken']))
     else:
         print('Не удалось обработать заказ!')
-        print('Ошибка ', result_order['ErrorReason'])
-        print(result_order['ErrorMessage'])
+        print('Ошибка {}'.format(result_order['ErrorReason']))
+        print('Текст ошибки: {0}'.format(result_order['ErrorMessage']))
 else:
-    print('Получен код возврата ', result_order.status_code)
+    print('Получен код возврата {0}'.format(result_order.status_code))
     print('Ошибка выполнения запроса!')
