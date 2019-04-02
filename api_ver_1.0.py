c3414eddead1dbc78e90a8793c8c1bdfecbb66f6
Alexey Nikiforov <kefir2204nikiforov@gmail.com>
2019-04-02 22:47:29 +0300

    change settings. Add missing parameters. Input value moved in json file

--- api_ver_1.0.py
+++ api_ver_1.0.py
@@ -4,6 +4,7 @@ import requests
 import json
 import configparser
 import os
+import sys
 
 
 
@@ -12,14 +13,13 @@ config._interpolation = configparser.ExtendedInterpolation()
 
 if not os.path.exists('settings.ini'):
     print('Не найден конфигурационный файл')
+    sys.exit()
 
 config.read('settings.ini')
+
+##### Получение телефонного номера
+
 URL = config.get('urls', 'URL')
-
-# получения номера телефона в регионе Москва
-
-URL_RES = config.get('urls', 'URL_RES')
-
 request_res = requests.get(URL) #выполняем запрос
 
 if request_res.status_code == 200:
@@ -39,6 +39,10 @@ else:
 
 
 ##### Резервирование номера
+
+# получения номера телефона в регионе Москва
+
+URL_RES = config.get('urls', 'URL_RES')
 list_number = []
 list_number.append(number)
 data = list_number
@@ -60,13 +64,86 @@ else:
     print('Ошибка выполнения запроса!')
 
 
-##### оформить заказ
+##### оформляем заказ
+
+##### изначально получаем город в регионе
+
+URL_CITY = config.get('urls', 'URL_CITY')
+request_city = requests.get(URL_CITY) #выполняем запрос
+
+if request_city.status_code == 200:
+    data_city = request_city.json()
+    #print(data_city)
+    if data_city['IsSuccess'] == True:
+        city_id = data_city['Data'][1]['Id']
+        print('Получен следующий уникальный идентификатор города {0}'.format(city_id))
+    else:
+        print('Не удалось получить город!')
+        print('Ошибка {0}'.format(data_city['ErrorReason']))
+        print('Текст ошибки: {0}'.format(data_city['ErrorMessage']))
+else:
+    print('Получен код возврата {0} '.format(request_city.status_code))
+    print('Ошибка выполнения запроса!')
+
+##### Получаем список точек продаж в городе
+
+URL_SAL = config.get('urls', 'URL_SAL')
+URL_SAL = '{0}{1}'.format(URL_SAL, city_id)
+#print(URL_SAL)
+request_sale = requests.get(URL_SAL) #выполняем запрос
+
+if request_sale.status_code == 200:
+    data_sale = request_sale.json()
+    #print(data_sale)
+    if data_sale['IsSuccess'] == True:
+        sale_id = data_sale['Data'][0]['Id']
+        print('Получен следующий уникальный идентификатор точки продаж {0}'.format(sale_id))
+    else:
+        print('Не удалось получить уникальный идентификатор точки продаж!')
+        print('Ошибка {0}'.format(data_sale['ErrorReason']))
+        print('Текст ошибки: {0}'.format(data_sale['ErrorMessage']))
+else:
+    print('Получен код возврата {0} '.format(request_sale.status_code))
+    print('Ошибка выполнения запроса!')
+
+##### Получаем тариф в регионе
+
+URL_TAR = config.get('urls', 'URL_TAR')
+request_tar = requests.get(URL_TAR) #выполняем запрос
+
+if request_tar.status_code == 200:
+    data_tar = request_tar.json()
+    #print(data_tar)
+    if data_tar['IsSuccess'] == True:
+        tarif_id = data_tar['Data'][0]['Id']
+        print('Получен следующий уникальный идентификатор тарифа {0}'.format(tarif_id))
+    else:
+        print('Не удалось получить уникальный идентификатор тарифа!')
+        print('Ошибка {0}'.format(data_tar['ErrorReason']))
+        print('Текст ошибки: {0}'.format(data_tar['ErrorMessage']))
+else:
+    print('Получен код возврата {0} '.format(request_tar.status_code))
+    print('Ошибка выполнения запроса!')
+
+
+#####
 URL_ORDER = config.get('urls', 'URL_ORDER')
-URL_ORDER = URL_ORDER + token
+URL_ORDER = '{0}{1}'.format(URL_ORDER, token)
+#print(URL_ORDER)
 # Входные данные
-with open('data.json', 'r', encoding='utf-8') as data:
-    input_data = json.load(data)
-print(input_data)
+with open('data.json', 'r', encoding='utf-8') as data_in:
+    input_data = json.load(data_in)
+
+for key in input_data:
+    if key == 'SalesPointId':
+        input_data[key] = sale_id
+    elif key == 'CityId':
+        input_data[key] = city_id
+    elif key == 'Products':
+        input_data[key][0]['Msisdn'] = number
+        input_data[key][0]['TariffId'] = tarif_id
+#print(input_data)
+
 data2_json = json.dumps(input_data)
 #print(data2_json)
 response_order = requests.post(URL_ORDER, data=data2_json, headers=headers)
@@ -74,7 +151,7 @@ if response_order.status_code == 200:
     result_order = response_order.json()
     #print(result_order)
     if result_order['IsSuccess'] == True:
-        print('Получен заказ {0}'.format(result_order['Data']['ReservationResult']['ReservationToken']))
+        print('Получен номер заказа {0}'.format(result_order['Data']['OrderNumber']))
     else:
         print('Не удалось обработать заказ!')
         print('Ошибка {}'.format(result_order['ErrorReason']))
